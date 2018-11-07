132321bd16e60263c4062ae0bb760bff8ebec521
OrangeC <604150318@qq.com>
2018-11-07 19:32:29 +0800

    demo.sh

---
+++ demo.sh
@@ -0,0 +1,18 @@
+if [ -d $1 ]; then
+	echo 'error: dir exists'
+	exit 
+else
+	mkdir $1
+	cd $1
+	mkdir css js
+	echo '<!DOCTYPE>'> index.html
+	echo '<title>Hello</title>' >> index.html
+	echo '<h1>Hi</h1>' >> index.html
+	cd css
+	echo 'h1{color: red;}' > style.css
+	cd ..
+	cd js
+	echo 'var string = "Hello World"' > main.js
+	echo 'alert(string)' >> main.js
+	exit
+fi
